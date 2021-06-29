ba81071a2a93789725de834282536b96f6851e8e
Christian12c <christianndjanda@gmail.com>
2021-06-29 17:00:46 +0100

    Add files via upload

---
+++ Sudoku Solver/Sudoku Solver.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Sudoku_Solver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.solveButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // solveButton
+            // 
+            this.solveButton.Location = new System.Drawing.Point(484, 62);
+            this.solveButton.Margin = new System.Windows.Forms.Padding(2);
+            this.solveButton.Name = "solveButton";
+            this.solveButton.Size = new System.Drawing.Size(76, 24);
+            this.solveButton.TabIndex = 0;
+            this.solveButton.Text = "Solve!";
+            this.solveButton.UseVisualStyleBackColor = true;
+            this.solveButton.Click += new System.EventHandler(this.solveButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.solveButton);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Sudoku Solver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button solveButton;
+    }
+}
+
